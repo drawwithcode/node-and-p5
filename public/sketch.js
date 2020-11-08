aa851dee40731dbb4999eea43c97c5e3a91a22fa
mikima <mikimauri@gmail.com>
2020-11-08 15:34:29 +0100

    Updated libraries and formatting

--- public/sketch.js
+++ public/sketch.js
@@ -1,47 +1,42 @@
-function preload(){
-  // put preload code here
-}
-
-var socket;
+// Create a new connection using socket.io (imported in index.html)
+let socket = io();
+// Define which function should be called when a new message
+// comes from the server with type "mouseBroadcast"
+socket.on("mouseBroadcast", otherMouse);
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
-  createCanvas(400,400);
-  background('red');
-  // Create a new connection using socket.io (imported in index.html)
-  socket = io();
-  // Define which function should be called when a new message
-  // comes from the server with type "mouseBroadcast"
-  socket.on('mouseBroadcast', newDrawing)
+  createCanvas(windowWidth, windowHeight);
+  background("red");
 }
+
 // Callback function called when a new message comes from the server
 // Data parameters will contain the received data
-function newDrawing(data){
-	console.log('received:', data)
-	noStroke();
-	fill('yellow');
-	ellipse(data.x, data.y, 20);
+function otherMouse(data) {
+  console.log("received:", data);
+  noStroke();
+  fill("yellow");
+  ellipse(data.x, data.y, 20);
 }
 
 function mouseDragged() {
-	console.log('sending: ',mouseX, mouseY);
-	noStroke();
-	fill(255);
+  console.log("sending: ", mouseX, mouseY);
+  noStroke();
+  fill(255);
 
-	// create an object containing the mouse position
-	var data = {
-		x: mouseX,
-		y: mouseY
-	}
-	// send the object to server,
-	// tag it as "mouse" event
-	socket.emit('mouse', data)
+  // create an object containing the mouse position
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+  // send the object to server,
+  // tag it as "mouse" event
+  socket.emit("mouse", message);
 
-	ellipse(mouseX, mouseY, 20)
+  ellipse(mouseX, mouseY, 20);
 }
 
 function draw() {
-  // put drawing code here
-  background(0,5);
+  // evert draw cycle, add a background with low opacity
+  // to create the "fade" effect
+  background(0, 5);
 }
